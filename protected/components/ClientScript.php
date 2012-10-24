0f961e4ed36cc5515d9f71818a7ceff3cdf64799
Jin Hu <bixuehujin@gmail.com>
2012-10-24 15:09:40 +0800

    init gitblog project.

---
+++ protected/components/ClientScript.php
@@ -0,0 +1,68 @@
+<?php
+class ClientScript extends  CClientScript{
+	
+	public $globalName;
+	
+	private $_settings = array();
+	
+	public function init(){
+		parent::init();
+		if (!$this->globalName) {
+			$this->globalName = 'Yii';
+		}
+	}
+	
+	
+	/**
+	 * 
+	 * @param string $groupName 设置分组名称
+	 * @param mixed $settings key-value
+	 */
+	public function registerSettings($groupName, $settings) {
+		$this->_settings[$groupName] = $settings;
+	}
+	
+	
+	public function settingsToScript() {
+		return 'var ' . $this->globalName . ' = '
+			. json_encode($this->_settings) . ';';
+	}
+	
+	
+	public function renderHead(&$output) {
+		$html='';
+		foreach($this->metaTags as $meta)
+			$html.=CHtml::metaTag($meta['content'],null,null,$meta)."\n";
+		foreach($this->linkTags as $link)
+			$html.=CHtml::linkTag(null,null,null,null,$link)."\n";
+		foreach($this->cssFiles as $url=>$media)
+			$html.=CHtml::cssFile($url,$media)."\n";
+		foreach($this->css as $css)
+			$html.=CHtml::css($css[0],$css[1])."\n";
+		if($this->enableJavaScript)
+		{
+			if(isset($this->scriptFiles[self::POS_HEAD]))
+			{
+				foreach($this->scriptFiles[self::POS_HEAD] as $scriptFile)
+					$html.=CHtml::scriptFile($scriptFile)."\n";
+			}
+		
+			if(isset($this->scripts[self::POS_HEAD]))
+				$html.=CHtml::script(implode("\n",$this->scripts[self::POS_HEAD]))."\n";
+			
+			if(!empty($this->_settings)) {
+				$html .= CHtml::script($this->settingsToScript() . "\n") . "\n";
+			}
+		}
+		
+		if($html!=='')
+		{
+			$count=0;
+			$output=preg_replace('/(<title\b[^>]*>|<\\/head\s*>)/is','<###head###>$1',$output,1,$count);
+			if($count)
+				$output=str_replace('<###head###>',$html,$output);
+			else
+				$output=$html.$output;
+		}
+	}
+}
---
+++ protected/config/main.php
@@ -0,0 +1,114 @@
+<?php
+
+// uncomment the following to define a path alias
+// Yii::setPathOfAlias('local','path/to/local-folder');
+
+// This is the main Web application configuration. Any writable
+// CWebApplication properties can be configured here.
+return array(
+	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'name'=>'侠古仁风的博客',
+
+	// preloading 'log' component
+	'preload'=>array('log'),
+
+	// autoloading model and component classes
+	'import'=>array(
+		'application.models.*',
+		'application.components.*',
+		'system.web.helpers.*',
+	),
+
+	'modules'=>array(
+		// uncomment the following to enable the Gii tool
+		/*
+		'gii'=>array(
+			'class'=>'system.gii.GiiModule',
+			'password'=>'Enter Your Password Here',
+			// If removed, Gii defaults to localhost only. Edit carefully to taste.
+			'ipFilters'=>array('127.0.0.1','::1'),
+		),
+		*/
+	),
+
+	// application components
+	'components'=>array(
+		'user'=>array(
+			// enable cookie-based authentication
+			'allowAutoLogin'=>true,
+		),
+			
+		'clientScript' => array(
+			'class' => 'ClientScript',
+			'coreScriptUrl' => 'assets',
+			'packages' => array(
+				'bootstrap' => array(
+						'css' => array('bootstrap/css/bootstrap.css'),
+				),
+				'bootstrap.responsive' => array(
+						'css' => array('bootstrap/css/bootstrap-responsive.css'),
+						'depends' => array('bootstrap')
+				),
+				'bootstrap.plugins' => array(
+						'js' => array('bootstrap/js/bootstrap.js'),
+						'depends' => array('bootstrap', 'jquery'),
+				),
+				'jquery.html5_upload' => array(
+						'js' => array('jquery/jquery.html5_upload.js'),
+						'depends' => array('jquery'),
+				),
+				'jquery.form' => array(
+						'js' => array('jquery/jquery.form.js'),
+						'depends' => array('jquery'),
+				),
+				'template' => array(
+						'js' => array('template.js'),
+				),
+			),
+		),
+		// uncomment the following to enable URLs in path-format
+		/*
+		'urlManager'=>array(
+			'urlFormat'=>'path',
+			'rules'=>array(
+				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
+				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+			),
+		),
+		*/
+		'db'=>array(
+			'connectionString' => 'mysql:host=localhost;dbname=gitblog',
+			'emulatePrepare' => true,
+			'username' => 'root',
+			'password' => 'sdyxzsdyxz',
+			'charset' => 'utf8',
+		),
+		'errorHandler'=>array(
+			// use 'site/error' action to display errors
+			'errorAction'=>'site/error',
+		),
+		'log'=>array(
+			'class'=>'CLogRouter',
+			'routes'=>array(
+				array(
+					'class'=>'CFileLogRoute',
+					'levels'=>'error, warning',
+				),
+				// uncomment the following to show log messages on web pages
+				/*
+				array(
+					'class'=>'CWebLogRoute',
+				),
+				*/
+			),
+		),
+	),
+
+	// application-level parameters that can be accessed
+	// using Yii::app()->params['paramName']
+	'params'=>array(
+		// this is used in contact page
+		'adminEmail'=>'webmaster@example.com',
+	),
+);
