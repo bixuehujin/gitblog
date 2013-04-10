db4a99b8ff8ca4d721a0c61c069e31d5d6b172ae
Jin Hu <bixuehujin@gmail.com>
2013-04-10 13:56:49 +0800

    Update the AR of User.

--- protected/models/User.php
+++ protected/models/User.php
@@ -1,69 +1,115 @@
 <?php
 /**
- * User model class file.
+ * User AR class file.
  * 
  * @author Jin Hu <bixuehujin@gmail.com>
+ * @since  2012-10-10
  */
 
 class User extends CActiveRecord {
 	
 	/**
-	 * @param string $className
+	 * Cahces of loaded user objects, indexed by uid.
+	 * 
+	 * @var User[]
+	 */
+	private static $cached      = array();
+	/**
+	 * Cahces of loaded user objects, indexed by username.
+	 *
+	 * @var User[]
+	 */
+	private static $cachedName  = array();
+	/**
+	 * Cahces of loaded user objects, indexed by email.
+	 *
+	 * @var User[]
+	 */
+	private static $cachedEmail = array();
+	
+	
+	/**
 	 * @return User
 	 */
-	static public function model($className = __CLASS__) {
+	public static function model($className = __CLASS__) {
 		return parent::model($className);
 	}
-	
 	
 	public function tableName() {
 		return 'user';
 	}
 	
-	
-	public function getByName($name) {
-		return $this->findByAttributes(array('username'=>$name));
-	}
-	
-	
-	public function getById($id) {
-		return $this->findByPk($id);
-	}
-	
 	public function beforeSave() {
-		
 		return parent::beforeSave();
 	}
 	
-	static public function getName($uid) {
+	/**
+	 * Load a user by its indentifier(uid, username, email) from database.
+	 * 
+	 * @param integer $identifier
+	 * @return User
+	 */
+	public static function load($identifier) {
+		if (is_numeric($identifier)) {
+			return self::loadByUid($identifier);
+		}else if (strpos($identifier, '@') !== false){
+			return self::loadByEmail($identifier);
+		}else {
+			return self::loadByName($identifier);
+		}
+	}
+	
+	protected static function loadByUid($uid) {
+		if (!isset(self::$cached[$uid])) {
+			self::$cached[$uid] = self::model()->findByPk($uid);
+		}
+		return self::$cached[$uid];
+	}
+	
+	protected static function loadByName($name) {
+		if (!isset(self::$cachedName[$name])) {
+			self::$cachedName[$name] = self::model()->findByAttributes(array('username' => $name));
+		}
+		return self::$cachedName[$name];
+	}
+	
+	protected static function loadByEmail($email) {
+		if (!isset(self::$cachedEmail[$email])) {
+			self::$cachedEmail[$email] = self::model()->findByAttributes(array('email' => $email));
+		}
+		return self::$cachedEmail[$email];
+	}
+	
+	
+	public static function getName($uid) {
 		$cache = StaticCache::getInstance(__CLASS__ . __FUNCTION__);
 		if($username = $cache->get($uid)) {
 			return $username;
 		}
-		$attributes = self::model()->find('uid=' . $uid);
+		$attributes = self::load($uid);
 		$username = $attributes->username;
 		$cache->set($uid, $username);
 		return $username;
 	}
 	
 	/**
-	 * check if specified user is exsit.
+	 * Checking if specified user is exsit.
 	 * 
 	 * @param integer $uid
 	 */
-	static public function checkExist($uid) {
+	public static function checkExist($uid) {
 		return (bool)self::model()->find('uid=' . $uid);
 	}
 	
 	/**
-	 * checking if given username is taken by others.
+	 * Checking if given username is taken by others.
 	 * 
 	 * @param string $name
 	 * @param mixed $uid the uid to check, used for changed user information, default to null. 
 	 * @return boolean
 	 */
-	public function isNameTaken($name, $uid = null) {
-		$user = $this->findByAttributes(array('username'=>$name));
+	public static function isNameTaken($name, $uid = null) {
+		$user = self::model()->findByAttributes(array('username'=>$name));
 		if ($uid == null) {
 			return (bool) $user;
 		}
@@ -73,14 +119,14 @@ class User extends CActiveRecord {
 	}
 	
 	/**
-	 * checking if given email is taken by others.
+	 * Checking if given email is taken by others.
 	 *
 	 * @param string $email
 	 * @param mixed $uid the uid to check, used for changed user address, default to null.
 	 * @return boolean
 	 */
-	public function isEmailTaken($email, $uid = null) {
-		$user = $this->findByAttributes(array('email'=>$email));
+	public static function isEmailTaken($email, $uid = null) {
+		$user = self::model()->findByAttributes(array('email'=>$email));
 		if ($uid == null) {
 			return (bool) $user;
 		}
