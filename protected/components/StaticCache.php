a204829a83be1b97906530d6102b25b6895efc93
Jin Hu <bixuehujin@gmail.com>
2013-05-19 21:14:09 +0800

    Update StaticCache component.

--- protected/components/StaticCache.php
+++ protected/components/StaticCache.php
@@ -1,21 +1,24 @@
 <?php
-
 /**
- * use PHP static variable to cache data.
- * @author hujin
- *
+ * StaticCache class file.
+ * 
+ * @author Jin Hu <bixuehujin@gmail.com>
  */
-class StaticCache {
-	static private $_cache = array();
-	static private $_instance;
-	static private $_ident;
+
+class StaticCache extends CComponent{
 	
-	private function __construct() {
-		
+	static private $_cache = array();
+	static private $_instances = array();
+	
+	private $_ident;
+	private $_currCache;
+	
+	private function __construct($ident) {
+		$this->_ident = $ident;
+		$this->_currCache = &self::$_cache[$ident];
 	}
 	
 	private function __clone(){
-		
 	}
 	
 	/**
@@ -23,41 +26,85 @@ class StaticCache {
 	 * @param string $ident
 	 * @return StaticCache
 	 */
-	static public function getInstance($ident) {
-		if(!isset(self::$_instance)) {
-			self::$_instance = new StaticCache();
+	public static function getInstance($ident) {
+		if(!isset(self::$_instances[$ident])) {
+			$instance = new StaticCache($ident);
 		}
-		self::$_ident = $ident;
-		return self::$_instance;
+		return self::$_instances[$ident] = $instance;
 	}
 	
 	
-	protected function getUniqeKey($key) {
-		return self::$_ident . $key;
-	}
-	
+	/**
+	 * Set a value to cache.
+	 * 
+	 * @param string $key
+	 * @param mixed  $value
+	 * @return boolean
+	 */
 	public function set($key, $value) {
-		self::$_cache[$key] = $value;
+		$this->_currCache[$key] = $value;
 		return true;
 	}
 	
+	/**
+	 * Get a value by key from cache.
+	 * 
+	 * @param string $key
+	 * @return mixed
+	 */
 	public function get($key) {
-		$ukey = $this->getUniqeKey($key);
-		if (isset(self::$_cache[$ukey])) {
-			return self::$_cache[$ukey];
+		if (isset($this->_currCache[$key])) {
+			return $this->_currCache[$key];
 		}
 		return null;
 	}
 	
+	/**
+	 * Check whether a key is cached.
+	 * 
+	 * @param string $key
+	 * @return boolean
+	 */
+	public function exists($key) {
+		return isset($this->_currCache[$key]);
+	}
+	
+	
+	/**
+	 * Remove a value by key from cache.
+	 * 
+	 * @param string $key
+	 * @return boolean
+	 */
+	public function remove($key) {
+		if (isset($this->_currCache[$key])) {
+			unset($this->_currCache[$key]);
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Clear all cached keys of current object.
+	 */
+	public function clear() {
+		$this->_currCache = array();
+	}
+	
+	/**
+	 * Get multiple values by keys.
+	 * 
+	 * @param array $keys
+	 * @return array indexed by its key.
+	 */
 	public function mget($keys) {
 		if(empty($keys)) {
 			return array();
 		}
 		$ret = array();
 		foreach($keys as $key) {
-			$ukey = $this->getUniqeKey($key);
-			if (isset(self::$_cache[$ukey])) {
-				$ret[$key] = self::$_cache[$ukey];
+			if (isset($this->_currCache[$key])) {
+				$ret[$key] = $this->_currCache[$key];
 			}else {
 				$ret[$key] = null;
 			}
@@ -65,10 +112,16 @@ class StaticCache {
 		return $ret;
 	}
 	
+	/**
+	 * Add a key=>value pairs to cache.
+	 * 
+	 * @param string $key
+	 * @param mixed  $value
+	 * @return boolean
+	 */
 	public function add($key, $value) {
-		$ukey = $this->getUniqeKey($key);
-		if(!isset(self::$_cache[$ukey])) {
-			self::$_cache[$ukey] = $value;
+		if(!isset($this->_currCache[$key])) {
+			$this->_currCache[$key] = $value;
 		}
 		return true;
 	}
